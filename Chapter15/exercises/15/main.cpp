24e9f75049d7cfc78af65d08494d7203776361e4
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2026-08-10 17:45:05 +0200

    Update main.

--- Chapter15/exercises/15/main.cpp
+++ Chapter15/exercises/15/main.cpp
@@ -1,6 +1,4 @@
-#define private public
 #include "Skiplist.hpp"
-#undef private
 
 #include <iostream>
 #include <string>
@@ -15,17 +13,32 @@ void print_list(const Skiplist<Key, T>& list)
 	}
 }
 
-constexpr char  PRINT_OP = 'p';
-constexpr char INSERT_OP = 'i';
-constexpr char  ERASE_OP = 'e';
+constexpr char    PRINT_OP = 'p';
+constexpr char   INSERT_OP = 'i';
+constexpr char   SEARCH_OP = 's';
+constexpr char CONTAINS_OP = 'c';
+constexpr char    ERASE_OP = 'e';
+constexpr char     HELP_OP = 'h';
 
-using Key   = int;
-using Value = int;
+void print_help()
+{
+	std::cout << "Usage:\n"
+		<< "\t'" << PRINT_OP << "'              : print elements\n"
+		<< "\t'" << INSERT_OP << "' <key> <value>: insert key-value pair\n"
+		<< "\t'" << SEARCH_OP << "' <key>        : search for key\n"
+		<< "\t'" << CONTAINS_OP << "' <key>        : check if key exists\n"
+		<< "\t'" << ERASE_OP << "' <key>        : erase pair with key\n"
+		<< "\t'" << HELP_OP << "'              : print help\n";
+}
+
+using Key   = std::string;
+using Value = std::string;
 
 int main()
 {
 	Skiplist<Key, Value> list{1};
-	for (char op; std::cin >> op; )
+	std::cout << "> ";
+	for (char op; std::cin >> op; std::cout << "> ")
 	{
 		switch (std::tolower(op))
 		{
@@ -42,16 +55,47 @@ int main()
 				}
 				break;
 			}
+			case SEARCH_OP:
+			{
+				Key key;
+				if (std::cin >> key)
+				{
+					Node<Key, Value>* found = list.search(key);
+					if (found)
+					{
+						std::cout << '[' << found->key() << "]: " << found->value() << '\n';
+					}
+					else
+					{
+						std::cout << "not found\n";
+					}
+				}
+				break;
+			}
+			case CONTAINS_OP:
+			{
+				Key key;
+				if (std::cin >> key)
+				{
+					bool found = list.contains(key);
+					std::cout << (found ? "found" : "not found") << '\n';
+				}
+				break;
+			}
 			case ERASE_OP:
 			{
 				Key key;
 				if (std::cin >> key)
 				{
-					list.erase(key);
+					bool erased = list.erase(key);
+					std::cout << (erased ? "found & erased" : "not found") << '\n';
 				}
 				break;
 			}
+			case HELP_OP:
+				[[fallthrough]];
 			default:
+				print_help();
 				break;
 		}
 	}
